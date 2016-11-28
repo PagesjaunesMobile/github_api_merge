c7db16d9f6d75d0d9714831ab6bad3366d76ec08
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2016-11-28 16:17:15 +0100

    feat(merge): log progress

--- step.rb
+++ step.rb
@@ -35,14 +35,24 @@ def log_done(message)
 end
 
 def export_output(out_key, out_value)
-  IO.popen("envman add --key #{out_key.to_s}", 'r+') { |f|
+ IO.popen("envman add --key #{out_key.to_s}", 'r+') { |f|
     f.write(out_value.to_s)
     f.close_write
     f.read
   }
+  while !process.eof?
+    line = process.gets
+    puts "GOT: #{line}"
+  end
 end
 
-
+def delete_branch? host
+  ENV["BITRISEIO_PULL_REQUEST_REPOSITORY_URL"].include? host
+end
+ 
+def reviewed? comments
+  comments.one? {|p| p.body.downcase.include? 'code review ok'}
+end
 
 log_info "init Merge"
 branch = ENV["BITRISE_GIT_BRANCH"]
@@ -51,27 +61,22 @@ repo_base = matches[1]
 repo = repo_base +  "/" + ENV["BITRISE_APP_TITLE"]
 pull_id = ENV["BITRISE_PULL_REQUEST"]
 authorization_token = ENV["auth_token"]
-if authorization_token.to_s.empty?
-  puts "No authorization_token specified"
-  exit 1
-end
 
-if pull_id.to_s.empty?
-  puts "No pull request specified"
-  exit 1
-end
+  log_fail "No authorization_token specified" if authorization_token.to_s.empty?
+
+  log_fail "No pull request specified" if pull_id.to_s.empty?
 
 client = Octokit::Client.new access_token:authorization_token
 
-comments =client.issue_comments repo , pull_id
-if comments.one? do |p| 
-   log_info p.body
-   p.body.downcase include? "code review ok"
- end
-  client.merge_pull_request repo, pull_id
+comments = client.issue_comments repo , pull_id
+
+log_info "reviewed :#{ reviewed? comments}"
+if reviewed? comments
+  client.merge_pull_request repo, pull_id 
   log_done "#{branch} merged"
   export_output "BITRISE_AUTO_MERGE_INFO", "#{branch} merged"
-  client.delete_ref branch if ENV["BITRISEIO_PULL_REQUEST_REPOSITORY_URL"].contains? repo_base
-else
-  export_output "BITRISE_AUTO_MERGE_INFO", "#{branch} merged"
+  log_info "deleted :#{delete_branch? repo_base}"
+  client.delete_ref branch if delete_branch? repo_base
 end
+
+  log_done "done"
