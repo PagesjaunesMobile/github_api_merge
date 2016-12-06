8c7b97c6389bdc960b4c1dc4583ac1d42d3f19a7
Michaël Duthilleul <mduthilleul@mac-pro.micro.pj.fr>
2016-12-06 10:16:33 +0100

    Minor Cleanup

--- step.rb
+++ step.rb
@@ -1,16 +1,5 @@
 require 'octokit'
 
-
-# --------------------------
-# --- Constants & Variables
-# --------------------------
-
-@this_script_path = File.expand_path(File.dirname(__FILE__))
-
-# --------------------------
-# --- Functions
-# --------------------------
-
 def log_fail(message)
   puts
   puts "\e[31m#{message}\e[0m"
@@ -42,13 +31,16 @@ def export_output(out_key, out_value)
   }
 end
 
-def delete_branch? host
+def delete_branch?(host)
   ENV["BITRISEIO_PULL_REQUEST_REPOSITORY_URL"].include? host
 end
- 
-def reviewed? comments
-  comments.one? {|p| p.body.downcase.include? 'code review ok'}
+
+def reviewed?(key, comments)
+  comments.one? {|p| p.body.downcase.include? key}
 end
+
+reviewed_key = ENV["reviewed_key"]
+auth_token = ENV["auth_token"]
 
 log_info "init Merge"
 branch = ENV["BITRISE_GIT_BRANCH"]
@@ -56,23 +48,24 @@ matches = /:([^\/]*)\//.match ENV["GIT_REPOSITORY_URL"]
 repo_base = matches[1]
 repo = repo_base +  "/" + ENV["BITRISE_APP_TITLE"]
 pull_id = ENV["BITRISE_PULL_REQUEST"]
-authorization_token = ENV["auth_token"]
+authorization_token = auth_token
 
-  log_fail "No authorization_token specified" if authorization_token.to_s.empty?
-
-  log_fail "No pull request specified" if pull_id.to_s.empty?
+log_fail "No authorization_token specified" if authorization_token.to_s.empty?
+log_fail "No pull request specified" if pull_id.to_s.empty?
 
 client = Octokit::Client.new access_token:authorization_token
 
 comments = client.issue_comments repo , pull_id
 
-log_info "reviewed :#{ reviewed? comments}"
-if reviewed? comments
-  client.merge_pull_request repo, pull_id 
+reviewed = reviewed?(reviewed_key, comments)
+
+log_info "reviewed :#{ reviewed }"
+if reviewed
+  client.merge_pull_request repo, pull_id
   log_done "#{branch} merged"
   export_output "BITRISE_AUTO_MERGE", "True"
-  log_info "deleted :#{delete_branch? repo_base}"
-  client.delete_branch repo, branch if delete_branch? repo_base
+  #log_info "deleted :#{delete_branch? repo_base}"
+  #client.delete_branch repo, branch if delete_branch? repo_base
 end
 
   log_done "done"
--- step.sh
+++ step.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$SCRIPT_DIR/libs/messages.sh"
+
+if [ ! -d "$auth_token" ]; then
+  msg_error "Auth token not found"
+	exit 1
+fi
+
+if [ ! -d "$reviewed_key" ]; then
+  msg_warn "Review comment key defaulted to 'code review ok'"
+	$reviewed_key="code review ok"
+fi
+
+
+
+msg_info "Installing Octikit"
 gem install octokit
-ruby "$THIS_SCRIPT_DIR/step.rb"
+
+msg_info "Executing script"
+ruby "$SCRIPT_DIR/step.rb"
 exit $?
