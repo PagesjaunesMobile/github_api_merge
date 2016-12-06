77a42c043c324076e906e936be217bd5991780f2
Michaël Duthilleul <mduthilleul@mac-pro.micro.pj.fr>
2016-12-06 16:25:32 +0100

    Fix arg check. Add bitrise test workflow

--- step.sh
+++ step.sh
@@ -5,14 +5,17 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/libs/messages.sh"
 
-if [ ! -d "$auth_token" ]; then
-  msg_error "Auth token not found"
+msg_info "auth_token: $auth_token"
+msg_info "reviewed_key: $reviewed_key"
+
+if [ -z "$auth_token" ]; then
+  msg_error "auth_token not found"
 	exit 1
 fi
 
-if [ ! -d "$reviewed_key" ]; then
-  msg_warn "Review comment key defaulted to 'code review ok'"
-	$reviewed_key="code review ok"
+if [ -z "$reviewed_key" ]; then
+  msg_warn "reviewed_key defaulted to 'code review ok'"
+	reviewed_key="code review ok"
 fi
 
 
