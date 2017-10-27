7f90279d1100144e1273133accdc19da307834eb
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2017-10-27 15:39:37 +0200

    feat(release): report automatique

--- step.rb
+++ step.rb
@@ -115,12 +115,16 @@ end
 if reviewed?(reviews, comments)
   begin
     log_details "#{repo}, #{pull_id}  #{options}"
-    client.merge_pull_request repo, pull_id ,'', options
+    resultMerge = client.merge_pull_request repo, pull_id ,'', options
     log_done "#{branch} merged #{options[:merge_method]}"
     export_output "BITRISE_AUTO_MERGE", "True"
     log_info "deleted :#{delete_branch? repo_base}"
     client.delete_branch repo, branch if delete_branch? repo_base
-    client.create_pull_request repo, "develop", "release", "chore(fix): report fixes",ENV["BITRISE_GIT_MESSAGE"] if dest == "release"
+    if dest == "release" && resultMerge.merged
+      new_branch = "heads/feat/reportRelease_#{pull_id}"
+      client.create_ref repo, new_branch, resultMerge.sha
+      client.create_pull_request repo, "develop", new_branch, "chore(fix): report fixes", "code review OK"
+    end
   rescue Octokit::MethodNotAllowed
     client.merge repo, branch, dest, :merge_method => "rebase" 
   end
