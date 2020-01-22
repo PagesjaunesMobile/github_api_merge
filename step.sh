4b0062143d8c70f1bcc1de9a989ca4cd863e66a8
CyrilPatrice <cpatrice@solocal.com>
2020-01-22 12:56:14 +0100

    debug

--- step.sh
+++ step.sh
@@ -2,9 +2,12 @@
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-set -e
+set +e
+set
+which ruby
 ruby -v
 gem -v
 gem install octokit
+set -e
 ruby "$THIS_SCRIPT_DIR/step.rb"
 exit $?
