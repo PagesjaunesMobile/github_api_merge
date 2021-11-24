419f1f66bc654a3b94b76104b5c312377c72e0ac
CyrilPatrice <cpatrice@solocal.com>
2021-11-24 11:00:19 +0100

    fix(trigger): infinite loop the current MR

--- step.rb
+++ step.rb
@@ -160,7 +160,7 @@ if reviewed?(reviews, comments, lastCommit)
   
   nextMR = client.merge_requests(repo, {state: "opened", approved_by_ids: "Any"}) 
   begin
-    client.rebase repo, nextMR.first.iid
+    client.rebase repo, nextMR..select { |item| item.iid != pull_id }.first.iid
   rescue => ex
     log_done "Nothing left to do ...#{ex}"
   end
